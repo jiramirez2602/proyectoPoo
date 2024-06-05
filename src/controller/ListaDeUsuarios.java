0ef70cdbe324f7cb6d9b9b3d7dde2d92f5baf9de
Windows <Windows@DESKTOP-52PJG9U>
2024-06-05 15:46:09 -0400

    upgrade usuarios

--- src/controller/ListaDeUsuarios.java
+++ src/controller/ListaDeUsuarios.java
@@ -5,81 +5,122 @@ import javax.swing.JOptionPane;
 import model.Usuario;
 
 public class ListaDeUsuarios {
-    ArrayList <Usuario> listaUsuarios;
+    ArrayList <Usuario> listaUsuarios=new ArrayList<>() ;
     
-    public void iniciarArraylistUsuarios(){
+    public void iniciarListaDeUsuarios(){
         listaUsuarios=new ArrayList();
     }
     
-    public void crearUsuario(ArrayList<String>privilegios,String nombre,String contraseña,String nombreCompleto,ArrayList<String>privilegio,String rol,boolean status){
-       boolean permitido=false;
-        for(String comprobar:privilegios){
-            if(comprobar.equals("Usuarios")){
-                permitido=true;
+    public boolean usuarioExistente(String username){
+        boolean existe=false;
+        for(int cont=0;cont<listaUsuarios.size();cont++){
+            if(listaUsuarios.get(cont).getUsername().equals(username)){
+                existe=true;
             }
         }
-        if(permitido==true){
-            Usuario usuario=new Usuario(nombre,contraseña,nombreCompleto,privilegio,rol,status);
-            listaUsuarios.add(usuario);
-        }
-        else{
-            JOptionPane.showInputDialog("EL USUARIO NO ESTA PERMITIDO");
-        }
+        return existe;
     }
     
-    public void listarUsuarios(ArrayList<String>privilegios){
-        for(Usuario i:listaUsuarios){
-            
-        }
-    }
-    
-    public void listarUsuario(ArrayList<String>privilegios,String id){
-        for(Usuario i:listaUsuarios){
-            
-        }
-    }
-    
-    public void modificarUsuarios(ArrayList<String>privilegios,String nombre,String nombreUsuarioNuevo,String contrasena,String nombrecompleto,ArrayList<String> privilegio,String rol,boolean status){
-        boolean permitido=false;
-        for(String comprobar:privilegios){
-            if(comprobar.equals("Usuarios")){
-                permitido=true;
+    public boolean crearUsuario(Usuario user, String username, String contrasena, String nombreCompleto, ArrayList<String> privilegio, String rol,String status) {
+        Validador validar = new Validador();//Declaro objeto Validador
+        boolean existe = usuarioExistente(username);//Declaro un booleano y le asigno una funcion para comprobar si un usuario existe en la lista
+        boolean retornar=false;
+        if (existe == true) {
+            JOptionPane.showMessageDialog(null, "Ya existe un usuario con el nombre:" + username + ". Intente con otro nombre", "Error", JOptionPane.ERROR_MESSAGE);
+        } else {
+            if (!validar.validarConRegex(username,"^.{1,30}$","Nombre del usuario","Limite de caracteres sobrepasado, solo se puede ingresar de 5 a 30 caracteres")
+                    || !validar.validarConRegex(contrasena,"^.{1,30}$", "Contraseña del usuario","Limite de caracteres sobrepasado, solo se puede ingresar de 5 a 30 caracteres")
+                    || !validar.validarConRegex(nombreCompleto,"^[a-zA-Z ]{1,30}$","Nombre Verdadero del usuario","El nombre es invalido, solo se aceptan caracteres alfabeticos, con un limite de 5 a 30 caracteres")
+                    || !validar.validarConRegex(rol,"^[a-zA-Z]{1,30}$","Rol del usuario","El rol es invalido, solo se aceptan caracteres alfabeticos, con un limite de 5 a 30 caracteres")
+                    || !validar.validarConRegex(status,"^(true|false)$","Estado del usuario","El estado es invalido")) {
+                return retornar;
+            }
+            else{
+                boolean estado=true;
+                try{
+                    estado=Boolean.parseBoolean(status);
+                }
+                catch(Exception e){
+                    estado=false;
+                }
+                Usuario usuario=new Usuario(username,contrasena,nombreCompleto,privilegio,rol,estado);
+                listaUsuarios.add(usuario);
+                retornar=true;
             }
         }
-        if(permitido==true){
-            for(int cont=0;cont<listaUsuarios.size();cont++){
-                if(listaUsuarios.get(cont).getUsername().equals(nombre)){
-                    listaUsuarios.get(cont).setUsername(nombreUsuarioNuevo);
-                    listaUsuarios.get(cont).setContrasena(contrasena);
-                    listaUsuarios.get(cont).setNombreUser(nombrecompleto);
-                    listaUsuarios.get(cont).setPrivilegios(privilegio);
-                    listaUsuarios.get(cont).setRolUsuario(rol);
-                    listaUsuarios.get(cont).setStatus(status);
+        return retornar;
+    }
+    
+    public ArrayList<Usuario> listarUsuarios(){
+        return listaUsuarios;
+    }
+    
+    public Usuario listarUsuario(String username){
+        for(Usuario encontrar:listaUsuarios){
+            if(encontrar.getUsername().equals(username)){
+                return encontrar;
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Usuario no encontrado", "Error", JOptionPane.ERROR_MESSAGE);
+        return null;
+    }
+    
+    public boolean modificarUsuarios(Usuario user,String oldUser,String newUser,String contrasena,String nombreCompleto,ArrayList<String> privilegio,String rol,String status){
+        Validador validar = new Validador();
+        Usuario usuarioExiste = listarUsuario(oldUser);
+        boolean existeNombre = false;
+        boolean retornar = false;
+        if (usuarioExiste != null) {
+            existeNombre = usuarioExistente(newUser);
+            if (existeNombre == true) {
+                JOptionPane.showMessageDialog(null, "Ya existe un usuario con el nombre:" + newUser + ". Intente con otro nombre", "Error", JOptionPane.ERROR_MESSAGE);
+                return retornar;
+            } else {
+                if (!validar.validarConRegex(newUser,"^.{1,30}$","Nombre del usuario","Limite de caracteres sobrepasado, solo se puede ingresar de 5 a 30 caracteres")
+                    || !validar.validarConRegex(contrasena,"^.{1,30}$", "Contraseña del usuario","Limite de caracteres sobrepasado, solo se puede ingresar de 5 a 30 caracteres")
+                    || !validar.validarConRegex(nombreCompleto,"^[a-zA-Z ]{1,30}$","Nombre Verdadero del usuario","El nombre es invalido, solo se aceptan caracteres alfabeticos, con un limite de 5 a 30 caracteres")
+                    || !validar.validarConRegex(rol,"^[a-zA-Z]{1,30}$","Rol del usuario","El rol es invalido, solo se aceptan caracteres alfabeticos, con un limite de 5 a 30 caracteres")
+                    || !validar.validarConRegex(status,"^(true|false)$","Estado del usuario","El estado es invalido")) {
+                    return retornar;
+                } else {
+                    boolean estado = true;
+                    try {
+                        estado = Boolean.parseBoolean(status);
+                    } catch (Exception e) {
+                        estado = false;
+                    }
+                    Usuario userModded = new Usuario(newUser, contrasena, nombreCompleto, privilegio, rol,estado);
+                    for (int cont = 0; cont < listaUsuarios.size(); cont++) {
+                        if (listaUsuarios.get(cont).getUsername().equals(oldUser)) {
+                            listaUsuarios.set(cont,userModded);
+                        }
+                    }
+                    retornar=true;
                 }
             }
+        } else {
+            JOptionPane.showMessageDialog(null, "Usuario no encontrado", "Error", JOptionPane.ERROR_MESSAGE);
+            return retornar;
         }
-        else{
-            JOptionPane.showInputDialog("EL USUARIO NO ESTA PERMITIDO");
-        }
+        return retornar;
     }
     
-    public void eliminarUsuarios(ArrayList<String>privilegios,String nombre){
-        boolean permitido=false;
-        for(String comprobar:privilegios){
-            if(comprobar.equals("Usuarios")){
-                permitido=true;
-            }
-        }
-        if(permitido==true){
+    public boolean eliminarUsuarios(Usuario user,String nombreBorrar){
+        Usuario usuarioExiste=listarUsuario(nombreBorrar);
+        boolean retornar = false;
+        if(usuarioExiste!=null){
             for(int cont=0;cont<listaUsuarios.size();cont++){
-                if(listaUsuarios.get(cont).getUsername().equals(nombre)){
+                if(listaUsuarios.get(cont).getUsername().equals(nombreBorrar)){
                     listaUsuarios.remove(cont);
                 }
             }
+            retornar=true;
         }
         else{
-            JOptionPane.showInputDialog("EL USUARIO NO ESTA PERMITIDO");
+            JOptionPane.showMessageDialog(null, "Usuario no encontrado", "Error", JOptionPane.ERROR_MESSAGE);
+            return retornar;
         }
+        return retornar;
     }
     
     public boolean iniciarSesion(String user,String contrasena){
@@ -87,10 +128,8 @@ public class ListaDeUsuarios {
         boolean permitirPass=false;
         boolean retornar=false;
         for(int cont=0;cont<listaUsuarios.size();cont++){
-            if(listaUsuarios.get(cont).getUsername().equals(user)){
+            if(listaUsuarios.get(cont).getUsername().equals(user)&&listaUsuarios.get(cont).getContrasena().equals(contrasena)){
                 permitirUser=true;
-            }
-            if(listaUsuarios.get(cont).getContrasena().equals(contrasena)){
                 permitirPass=true;
             }
         }
